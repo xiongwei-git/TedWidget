c137a6ea6bd6061482c05689b337706a8f343a52
Ted <xiong-wei@hotmail.com>
2014-08-11 17:40:06 +0800

    add TImageView
    add TImageView

---
+++ src/com/android/TedWidget/view/TImageView.java
@@ -0,0 +1,23 @@
+package com.android.TedWidget.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Ted on 14-8-11.
+ */
+public class TImageView extends ImageView{
+
+    public TImageView(Context context) {
+        super(context);
+    }
+
+    public TImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
